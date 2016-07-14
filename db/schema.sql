99f114f3a819971fabc1fa9419c557e5b12608ad
erin cherney <erin.t.cherney@gmail.com>
2016-07-14 14:29:39 -0400

    db made

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks(
+  task_id SERIAL PRIMARY KEY NOT NULL,
+  task_name VARCHAR(64) NOT NULL,
+  task_desc TEXT,
+  completed BOOLEAN NOT NULL DEFAULT FALSE,
+  task_time_start TIMESTAMP,
+  task_time_end TIMESTAMP,
+  task_created TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE INDEX on tasks (completed);
+CREATE INDEX on tasks (task_time_start);
+CREATE INDEX on tasks (task_created);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO tasks (task_name, task_desc)
+  VALUES
+  ('wash the car', 'make sure to use soap'),
+  ('wash the baby', 'make sure to use soap'),
+  ('have a party', NULL);
